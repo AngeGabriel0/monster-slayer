256dc506573a90d435b45932d1f5fa6667f77428
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-06-12 13:39:26 +0200

    docs: adding code transpiled by typescript

--- dist/script.js
+++ dist/script.js
@@ -19,31 +19,36 @@ var Game = /** @class */ (function () {
         this.level = level;
         switch (this.level) {
             case 'easy':
-                this.attackPoints *= 5;
-                this.specialAttackPoints *= 10;
-                this.healPoints *= 3;
+                this.attackPoints *= 2;
+                this.specialAttackPoints *= 4;
+                this.healPoints *= 2;
                 break;
             case 'medium':
-                break;
-            case 'hard':
-                this.attackPoints *= 0.8;
+                this.attackPoints *= 1.25;
                 this.specialAttackPoints *= 2;
-                this.healPoints = 0;
+                this.healPoints *= 1.5;
                 this.monsterLifePoints = 150;
                 monsterLifePointsDisplay.innerHTML = '150';
                 break;
+            case 'hard':
+                this.attackPoints *= 1;
+                this.specialAttackPoints *= 1.5;
+                this.healPoints *= 1.5;
+                this.monsterLifePoints = 200;
+                monsterLifePointsDisplay.innerHTML = '200';
+                break;
             case 'impossible':
-                this.attackPoints *= 0.6;
-                this.specialAttackPoints *= 4;
-                this.healPoints /= 1.5;
-                this.monsterLifePoints = 250;
-                monsterLifePointsDisplay.innerHTML = '250';
+                this.attackPoints *= 0.8;
+                this.specialAttackPoints *= 1;
+                this.healPoints *= 2;
+                this.monsterLifePoints = 350;
+                monsterLifePointsDisplay.innerHTML = '350';
                 break;
         }
     }
     Game.prototype.attack = function () {
-        var damage = Math.floor(Math.random() * 5) + 2;
-        this.lifePoints = Math.floor(this.lifePoints - (damage * 0.3));
+        var damage = Math.floor(Math.random() * this.attackPoints) + 1;
+        this.lifePoints = Math.floor(this.lifePoints - damage * 0.5);
         this.monsterLifePoints = Math.floor(this.monsterLifePoints - damage);
         // Limit the life points to 0 and 100
         this.lifePoints = Math.max(0, this.lifePoints);
@@ -55,7 +60,7 @@ var Game = /** @class */ (function () {
         this.result(); //Verify if the game is over (LP at 0)
     };
     Game.prototype.specialAttack = function () {
-        var damage = Math.floor(Math.random() * this.specialAttackPoints) + 4;
+        var damage = Math.floor(Math.random() * this.specialAttackPoints) + 2;
         this.lifePoints = Math.floor(this.lifePoints - damage * 0.3);
         this.monsterLifePoints = Math.floor(this.monsterLifePoints - damage);
         this.lifePoints = Math.max(0, this.lifePoints);
@@ -76,38 +81,37 @@ var Game = /** @class */ (function () {
             lifePointsDisplay.textContent = this.lifePoints.toString();
         }
         else if (this.lifePoints <= 0) { // If life points are at 0
-            alert('You lost!');
             attackButton.disabled = true;
             specialAttackButton.disabled = true;
             healBtn.disabled = true;
             resignBtn.disabled = true;
+            confirm('You lost ! New game ?');
             location.reload();
         }
         else {
-            alert("You already have full life points!");
+            alert("You already have full life points !");
         }
         lifePointsDisplay.style.width = this.lifePoints + '%';
     };
     Game.prototype.result = function () {
         if (this.lifePoints <= 0) {
-            alert('You lost! New game ?');
             attackButton.disabled = true;
             specialAttackButton.disabled = true;
             healBtn.disabled = true;
             resignBtn.disabled = true;
+            alert('You lost ! New game ?');
             location.reload(); //Reload the page
         }
         else if (this.monsterLifePoints <= 0) {
-            alert('You won! New game ?');
             attackButton.disabled = true;
             specialAttackButton.disabled = true;
             healBtn.disabled = true;
             resignBtn.disabled = true;
+            alert('You won ! New game ?');
             location.reload();
         }
     };
     Game.prototype.resign = function () {
-        alert('You resigned! New game ?');
         attackButton.disabled = true;
         specialAttackButton.disabled = true;
         healBtn.disabled = true;
@@ -115,44 +119,75 @@ var Game = /** @class */ (function () {
         monsterLifePointsDisplay.textContent = '0';
         lifePointsDisplay.style.width = '0';
         monsterLifePointsDisplay.style.width = '0';
+        alert('You resigned... New game ?');
+        location.reload();
     };
     return Game;
 }());
-var level = prompt('Choose your level: easy, medium, hard, impossible');
+var level = prompt('Choose your level : easy, medium, hard, impossible');
 while (level !== 'easy' && level !== 'medium' && level !== 'hard' && level !== 'impossible') {
-    level = prompt('Choose your level: easy, medium, hard, impossible');
+    level = prompt('Choose your level : easy, medium, hard, impossible');
 }
 var newGame;
-if (level === 'easy' || level === 'medium') {
-    newGame = new Game(100, 100, 10, 15, 10, level);
+if (level === 'easy') {
+    document.querySelector('.monster h2').textContent = "Placidusax's health";
+    newGame = new Game(100, 100, 10, 10, 10, level);
 }
-else if (level === 'hard') {
-    newGame = new Game(100, 150, 15, 15, 10, level);
-    //Desactivate the special attack button after 5 clicks
-    var clickCount_1 = 0;
-    specialAttackButton.addEventListener('click', function () {
-        clickCount_1++;
-        if (clickCount_1 === 5) {
-            specialAttackButton.disabled = true;
-            specialAttackButton.style.backgroundColor = 'red';
-        }
-    });
-}
-else if (level === 'impossible') {
-    newGame = new Game(100, 300, 20, 15, 10, level);
-    //Desactivate the special attack and the heal button after 3 clicks
+else if (level === 'medium') {
+    document.querySelector('.monster h2').textContent = "Loretta's health";
+    newGame = new Game(100, 150, 10, 10, 10, level);
     var clickCountOnSpecialAttackBtn_1 = 0;
     var clickCountOnHealBtn_1 = 0;
     specialAttackButton.addEventListener('click', function () {
         clickCountOnSpecialAttackBtn_1++;
-        if (clickCountOnSpecialAttackBtn_1 === 3) {
+        if (clickCountOnSpecialAttackBtn_1 === 7) {
             specialAttackButton.disabled = true;
             specialAttackButton.style.backgroundColor = 'red';
         }
     });
     healBtn.addEventListener('click', function () {
         clickCountOnHealBtn_1++;
-        if (clickCountOnHealBtn_1 === 3) {
+        if (clickCountOnHealBtn_1 === 7) {
+            healBtn.disabled = true;
+            healBtn.style.backgroundColor = 'red';
+        }
+    });
+}
+else if (level === 'hard') {
+    document.querySelector('.monster h2').textContent = "Maliketh's health";
+    newGame = new Game(100, 200, 10, 10, 10, level);
+    var clickCountOnSpecialAttackBtn_2 = 0;
+    var clickCountOnHealBtn_2 = 0;
+    specialAttackButton.addEventListener('click', function () {
+        clickCountOnSpecialAttackBtn_2++;
+        if (clickCountOnSpecialAttackBtn_2 === 4) {
+            specialAttackButton.disabled = true;
+            specialAttackButton.style.backgroundColor = 'red';
+        }
+    });
+    healBtn.addEventListener('click', function () {
+        clickCountOnHealBtn_2++;
+        if (clickCountOnHealBtn_2 === 4) {
+            healBtn.disabled = true;
+            healBtn.style.backgroundColor = 'red';
+        }
+    });
+}
+else if (level === 'impossible') {
+    document.querySelector('.monster h2').textContent = "Malenia's health";
+    newGame = new Game(100, 350, 10, 10, 10, level);
+    var clickCountOnSpecialAttackBtn_3 = 0;
+    var clickCountOnHealBtn_3 = 0;
+    specialAttackButton.addEventListener('click', function () {
+        clickCountOnSpecialAttackBtn_3++;
+        if (clickCountOnSpecialAttackBtn_3 === 3) {
+            specialAttackButton.disabled = true;
+            specialAttackButton.style.backgroundColor = 'red';
+        }
+    });
+    healBtn.addEventListener('click', function () {
+        clickCountOnHealBtn_3++;
+        if (clickCountOnHealBtn_3 === 3) {
             healBtn.disabled = true;
             healBtn.style.backgroundColor = 'red';
         }
